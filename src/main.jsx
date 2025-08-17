444203e3a743608a9fde5b4a970e74a3353eca23
younghwan22 <164490271+younghwan22@users.noreply.github.com>
2025-08-17 22:28:19 +0900

    태경오빠부분까지

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import Home from './pages/Home.jsx'
-import AI from './pages/AI.jsx'
-import CASH from './pages/CASH.jsx'
-import CHAT from './pages/CHAT.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './pages/Home.jsx';
+import AI from './pages/AI.jsx';
+import CASH from './pages/CASH.jsx';
+import CHAT from './pages/CHAT.jsx';
+import ChartResult from './pages/CHART_RESULT.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -18,8 +19,9 @@ createRoot(document.getElementById('root')).render(
         <Route path="/ai" element={<AI />} />
         <Route path="/cash" element={<CASH />} />
         <Route path="/chat" element={<CHAT />} />
+        <Route path="/chart-result" element={<ChartResult />} /> 
       </Routes>
       <Footer />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
